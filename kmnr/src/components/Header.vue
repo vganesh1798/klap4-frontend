1e755d1657ad0129f8c4ed28d5f7dbe0bf64525a
Akshith Gara <akshithgara@Akshiths-MacBook-Pro.local>
2020-02-25 01:14:53 -0600

    Initial build with simple animation and all required routes [ag]

---
+++ kmnr/src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="Header">
+    <nav>
+
+        <router-link to="/"><img src="../../src/assets/radio.png" class="logo"></router-link>
+
+        <router-link to="/Blog" class="items">Blog</router-link>
+        <router-link to="/Schedule" class="items">Schedule</router-link>
+        <router-link to="/Djs" class="items">DJ's</router-link>
+        <router-link to="/News" class="items">News</router-link>
+        <router-link to="/Charts" class="items">Charts</router-link>
+        <router-link to="/Playlists" class="items">Playlists</router-link>
+        <router-link to="/About" class="items">About</router-link>
+
+    </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style lang="scss">
+    #Header nav {
+        width: 95%;
+        color: white;
+        min-height: 50px;
+        font-family: 'Arvo';
+    }
+
+    #Header .logo {
+        margin-left: 1.5em;
+        margin-top: 0.6em;
+        float: left;
+        height: 150px;
+
+    }
+
+    #Header .items{
+        margin-top: 1.4em;
+        float: right;
+        margin-left: 35px;
+        outline: none;
+        font-size: 20px;
+        color: white;
+
+        text-decoration: none;
+            &:hover {
+            outline: none;
+            color: #ff4d5a;
+            text-decoration: none;
+            }
+            &:focus {
+            outline: none;
+            color: #ff4d5a;
+            text-decoration: none;
+            }
+
+            &:actived {
+            outline: none;
+            color: #ff4d5a;
+            text-decoration: none;
+            }
+    }
+</style>
---
+++ kmnr/src/components/HomePage.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="text">
+            <h1 class="heading-primary">
+                <span class="heading-primary-main">KMNR</span>
+                <span class="heading-primary-sub">89.7FM</span>
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home-page"
+    }
+</script>
+
+<style >
+    .text {
+        position: absolute;
+        top: 45%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center; }
+
+    .heading-primary-main {
+        display: block;
+        font-size: 60px;
+        font-weight: 100;
+        animation-name: moveInleft;
+        animation-duration: 2s;
+        color: white;
+        font-family: Montserrat;
+    }
+
+    .heading-primary-sub {
+        display: block;
+        font-size: 16.5px;
+
+        font-family: Copperplate;
+        margin-top: 10px;
+        animation-name: moveInright;
+        animation-duration: 2s;
+        color: white; }
+
+    @keyframes moveInleft {
+        0% {
+            opacity: 0;
+            transform: translateX(-20px); }
+        100% {
+            opacity: 1;
+            transform: translate(0); } }
+
+    @keyframes moveInright {
+        0% {
+            opacity: 0;
+            transform: translateX(20px); }
+        100% {
+            opacity: 1;
+            transform: translate(0); } }
+
+
+</style>
