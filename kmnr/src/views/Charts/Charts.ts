fbe394f04558d40a7fb38946d1740deb5c32ce9b
sgriffin08 <43181537+sgriffin08@users.noreply.github.com>
2020-03-10 18:12:28 -0500

    updated charts page

--- kmnr/src/views/Charts/Charts.ts
+++ kmnr/src/views/Charts/Charts.ts
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-//import '../node_modules/materialize-css/dist/css/materialize.min.css';
-//import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
 export default Vue.extend({
     name: 'charts-page'
--- kmnr/src/views/Charts/Charts.vue
+++ kmnr/src/views/Charts/Charts.vue
@@ -1,118 +1,83 @@
 <template>
-    <div class="charts-page">
-      <img class="back" src="../../../public/testbackground.jpg" />
-      <div class="container charts-content">
-        <!--h2>Explore Charts</h2-->
-        <h2>Explore Charts</h2>
-          <div class="row">
-            <div class="col l3">
-              <ul class="tabs">
-                <li class="button"><a href="#test1">Top Albums</a></li>
-                <li class="button"><a href="#test2">Top Songs</a></li>
-                <li class="button"><a href="#test3">Top Artists</a></li>
-                <li class="button"><a href="#test4">Top Other</a></li>
-              </ul>
-            </div>
-            <div class="col l12">
-              <div class="row">
-                <div class="input-field col s12">
-                  <!--i class="material-icons prefix">textsms</i--->
-                  <input type="text" id="autocomplete-input" class="autocomplete">
-                  <label for="autocomplete-input">Search for an artist, genre, or album</label>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col l12">
-              <table class="highlight striped">
-                <thead>
-                  <tr>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  <tr>
-                    <td>song1</td>
-                    <td>artist1</td>
-                    <td>album1</td>
-                  </tr>
-                  <tr>
-                    <td>song2</td>
-                    <td>artist2</td>
-                    <td>album2</td>
-                  </tr>
-                  <tr>
-                    <td>song3</td>
-                    <td>artist3</td>
-                    <td>album3</td>
-                  </tr>
-                  <tr>
-                    <td>song4</td>
-                    <td>artist4</td>
-                    <td>album4</td>
-                  </tr>
-                  <tr>
-                    <td>song5</td>
-                    <td>artist5</td>
-                    <td>album5</td>
-                  </tr>
-                </tbody>
-              </table>
-          </div>
-        </div>
-        <!---ul class="tabs">
-          <li class="button"><a href="#test1">Top Albums</a></li>
-          <li class="button"><a class="active" href="#test2">Top Songs</a></li>
-          <li class="button"><a href="#test3">Top Artists</a></li>
-          <li class="button"><a href="#test4">Top Other</a></li>
-        </ul--->
-          <!---/div>
-          <div id="test1" class="col s12">Test 1</div>
-          <div id="test2" class="col s12">Test 2</div>
-          <div id="test3" class="col s12">Test 3</div>
-          <div id="test4" class="col s12">Test 4</div>
-        </div--->
+  <div id="charts-page">
+    <img class="back" src="../../../public/back.jpg" />
+    <h1 class="heading-charts">
+      <span class="heading-charts-main">Explore Charts</span>
+      <div class="container">
         <div class="row">
-          <div class="col l12">
-            <table id="secondTable" class="highlight striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Artist</th>
-                  <th>Album</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in topCharts" v-bind:key="item.id">
-                  <td v-for="col in columns" v-bind:key="col.id">{{ item[col] }}</td>
-                </tr>
-              </tbody>
-            </table>
+          <div class="col-lg-3"></div>
+          <div class="col-lg-2">
+            <button type="button" class="btn">New Charts</button>
+          </div>
+          <div class="col-lg-2">
+            <button type="button" class="btn">Top charts</button>
+          </div>
         </div>
       </div>
-      </div>
+    </h1>
+
+    <div class="container">
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Recommended</th>
+            <th scope="col">Album</th>
+            <th scope="col">Artist</th>
+            <th scope="col">Runtime</th>
+            <th scope="col">Album Tag</th>
+            <th scope="col">Artist Tag</th>
+            <th scope="col">Release Date</th>
+            <th scope="col">Total Plays</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in topCharts" v-bind:key="item.id">
+              <!--button type="button" class="btn">
+                <p v-if="item['Recommended']">rec</p>
+                <p v-else>not rec</p>
+              </button--->
+              <!--img class="recpic" v-show="item['Recommended']" src="../../assets/logo.png"/-->
+            <td v-for="col in columns" v-bind:key="col.id">
+              <p v-if="col==='Recommended'">
+                <!---img class="recpic" v-show="item['Recommended']" src="../../assets/logo.png"/--->
+                <button class="circlebtn" v-on:click="item['Recommended'] = !item['Recommended']">
+                  <img class="recpic" v-show="item['Recommended']" src="../../../public/star_button.jpg"/>
+                </button>
+              </p>
+              <p v-else>
+                {{ item[col] }}
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
+<script lang='ts' src="./Charts.ts" />
+
+
 <script>
-"./Charts.js"
-//import '../../../node_modules/materialize-css/dist/css/materialize.min.css';
-//import '../../../node_modules/materialize-css/dist/js/materialize.min.js';
+"./Charts.ts"
 
 export default {
     data () {
         return {
-            msg: 'helloo',
-            columns: ['name', 'artist', 'album'],
+            columns: ['Recommended', 'Album', 'Artist', 'Runtime', 'Album Tag', 'Artist Tag', 'Release Date', 'Total Plays'],
             topCharts: [
-               { name: 'song1', artist: 'artist1', album: '1' },
-               { name: 'song2', artist: 'artist2', album: '2' },
-               { name: 'song3', artist: 'artist3', album: '3' },
-               { name: 'song4', artist: 'artist4', album: '4' }
+               { Recommended: true, Album: 'album1', Artist: 'artist1', Runtime: '354', 'Album Tag': '1234', 'Artist Tag': '3384', 'Release Date': '12/2/2009', 'Total Plays': '245' },
+               { Recommended: false, Album: 'album2', Artist: 'artist2', Runtime: '213', 'Album Tag': 'RM2T5', 'Artist Tag': 'CO329F', 'Release Date': '2/22/2003', 'Total Plays': '244' },
+               { Recommended: true, Album: 'album3', Artist: 'artist3', Runtime: '100', 'Album Tag': '3843', 'Artist Tag': '39CL', 'Release Date': '8/13/2011', 'Total Plays': '214' },
+               { Recommended: true, Album: 'album4', Artist: 'artist4', Runtime: '298', 'Album Tag': 'RXO20', 'Artist Tag': '24SLS', 'Release Date': '6/28/1976', 'Total Plays': '198' },
+               { Recommended: false, Album: 'album5', Artist: 'artist5', Runtime: '245', 'Album Tag': 'ZO22', 'Artist Tag': '3245', 'Release Date': '3/2/1994', 'Total Plays': '194' },
+               { Recommended: false, Album: 'album6', Artist: 'artist6', Runtime: '198', 'Album Tag': '9284', 'Artist Tag': '49042', 'Release Date': '12/12/2002', 'Total Plays': '187' },
+               { Recommended: false, Album: 'album7', Artist: 'artist7', Runtime: '412', 'Album Tag': '1104', 'Artist Tag': '2393', 'Release Date': '1/26/2000', 'Total Plays': '175' },
+               { Recommended: true, Album: 'album8', Artist: 'artist8', Runtime: '190', 'Album Tag': 'K203L', 'Artist Tag': 'K2DSA0', 'Release Date': '8/19/1970', 'Total Plays': '164' },
+               { Recommended: true, Album: 'album9', Artist: 'artist9', Runtime: '245', 'Album Tag': 'CSOS2', 'Artist Tag': '30931', 'Release Date': '9/2/2001', 'Total Plays': '160' },
+               { Recommended: false, Album: 'album10', Artist: 'artist10', Runtime: '198', 'Album Tag': '2948', 'Artist Tag': 'XCO333', 'Release Date': '4/29/2009', 'Total Plays': '140' },
+               { Recommended: true, Album: 'album11', Artist: 'artist11', Runtime: '412', 'Album Tag': '10333', 'Artist Tag': 'OIO233', 'Release Date': '6/7/2014', 'Total Plays': '110' },
+               { Recommended: false, Album: 'album12', Artist: 'artist12', Runtime: '190', 'Album Tag': '292', 'Artist Tag': '290923', 'Release Date': '1/2/2000', 'Total Plays': '106' }
             ]
         }
     }
@@ -120,38 +85,91 @@ export default {
 </script>
 
 <style>
+.heading-charts-main {
+    display: block;
+    font-size: 100px;
+    font-weight: 100;
+    padding-top: 3%;
+    padding-left: 35%;
+    padding-bottom: 1%;
+    color: Black;
+    font-family: 'Covered By Your Grace';
+}
+
+.heading-charts-sub {
+    display: block;
+    text-align: center;
+    font-size: 25px;
+    padding-top: 0%;
+    font-family: Montserrat;
+    margin-top: 10px;
+    color: black; }
+
 .back {
     position: fixed;
     z-index: -1;
 
     /* Allows the background pic to not have any white lines on edge */
-    transform: scale(2);
+    transform: scale(1.3);
     -webkit-filter: scale(1.1);
     -moz-filter: scale(1.1);
     -o-filter: scale(1.1);
     -ms-filter: scale(1.1);
 }
 
-.charts-content {
-    padding-top: 10%;
-    text-align: center;
-    height: 100em;
-    z-index: -1;
+.recpic {
+  transform: scale(.8);
+  height: 25px;
 }
 
-.card-title {
-  font-size: 36px;
+.btn {
+    background-color: gray;
+    border-radius: 20px;
+    width: 150px;
+    font-family: Montserrat;
 }
 
-.card-text {
-
+.circlebtn {
+  border: none;
+  border-radius: 50%;
+  opacity: 40%;
+  height: 30px;
+  width: 30px;
 }
-.card {
-  width: 325px;
-  height: 300px;
-  text-align: center;
+
+.red {
+  background-color: red;
+}
+
+.green {
+  background-color: green;
+}
+
+.table {
+  color: black;
+  width: 1100px;
   font-size: 16px;
-  border-radius: 15px;
+  font-family: Montserrat;
 }
 
+tbody > tr {
+  height: 20px;
+}
+
+.table-hover > tbody > tr:hover {
+  /*background-color: #595959 !important;*/
+  background-color: #566D7E !important;
+}
+
+.table-striped > tbody > tr:nth-child(even){
+  background-color:  #a0cfec;
+  /*background-color: #AFDCEC;*/
+  /*background-color: #1569C7;*/
+}
+
+.table-striped > tbody > tr:nth-child(odd){
+  /*background-color:#0099cc;*/
+  background-color: #659EC7;
+  /*background-color: #6495ED;*/
+}
 </style>
