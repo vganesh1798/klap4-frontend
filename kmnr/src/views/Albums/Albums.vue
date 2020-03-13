7b89092fb24835e9c6af6c4268211703d763ef6d
Akshith Gara <akshithgara@Akshiths-MacBook-Pro.local>
2020-03-13 17:11:20 -0500

    Adjusted background image [ag]

--- kmnr/src/views/Albums/Albums.vue
+++ kmnr/src/views/Albums/Albums.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="Albums-page">
+      <img class="back" src="../../../public/back.jpg" />
       Routing works!
   </div>
 </template>
@@ -11,5 +12,14 @@ export default {
 }
 </script>
 <style>
-
+.back {
+    position: fixed;
+    z-index: -1;
+    /* Allows the background pic to not have any white lines on edge */
+    transform: scale(1.3);
+    -webkit-filter: scale(1.1);
+    -moz-filter: scale(1.1);
+    -o-filter: scale(1.1);
+    -ms-filter: scale(1.1);
+}
 </style>
