58dd34b5b839fc5f87d8f223e1fe8f7ff6fab1fd
Akshith Gara <akshithgara@Akshiths-MacBook-Pro.local>
2020-03-16 11:22:50 -0500

    Added album title info [ag]

--- kmnr/src/views/Albums/AlbumDetail/Albumdeets.vue
+++ kmnr/src/views/Albums/AlbumDetail/Albumdeets.vue
@@ -1,19 +1,59 @@
 <template>
-    <div> 
-    <img class="back" src="../../../../public/back.jpg" />
-    <h1 class = "albums-heading">
-          <span class="album-heading-main">{{album['im:name'].label}}</span>
-          
-    <div class="main_container">
-
-      <div class="row justify-content-between">
-          <div class="albums_image">
-            <img :src="album['im:image'][2].label">
+    <div id="Albums-page">
+      <img class="back" src="../../../../public/back.jpg" />
+      <h1 class = "albums-heading">
+          <span class="albums-heading-main">{{album['im:name'].label}}</span>
+    <!-- <div class="container">
+      
+            <div class="row justify-content-center" id="search_bar">
+                <div class="col-6">
+                    <input v-model="albumSearch" type="text" class="form-control" placeholder="Search by album name">             
+                </div>
+                <div class="col-1" id="submit">
+                    <button class="btn btn-light" @click="SearchByAlbumName()"><img src="https://img.icons8.com/color/24/000000/guitar-amp.png"></button>          
+                </div>
+                                <div class="col-5">
+                    <a :style="{visibility:cancelSearchVisibility}" class="cancelSearch" href="#" @click.prevent="CancelSearch()"> Cancel search </a>
+                </div>
             </div>
-      </div>
-    </div>
-    </h1>
-    </div>
+
+
+            <div class="row justify-content-between">
+                <div class="col-3 offset-2">
+                  <h3 class="albums_header">Albums</h3>  
+                </div>
+                            <div class="container_pagination">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                            <li class="page-item"><a class="page-link" @click="previousAlbums()"><img src="https://img.icons8.com/plasticine/64/000000/back.png"/></a></li>
+                            <li class="page-item"><a class="page-link" @click="nextAlbums()"><img src="https://img.icons8.com/plasticine/64/000000/forward.png"/></a></li>
+                    </ul>
+                </nav>
+        </div>
+
+            </div>
+
+
+        <div class="row justify-content-center">
+            <div class="col-8"> 
+                    <div class="d-flex justify-content-start flex-wrap albums" id="albums_container">
+                    <display v-for="album in albumsPaginated" 
+                            :key="album.id.attributes.id" 
+                            :id-album="album.id.attributes['im:id']"
+                            :album="album">
+                    </display>    
+                    
+                    </div>
+
+            </div>
+        </div>
+ 
+       -->
+
+        <!-- </div> -->
+      </h1>
+  </div>
+ 
 </template>
 
 <script>
@@ -45,31 +85,52 @@ export default {
     -o-filter: scale(1.1);
     -ms-filter: scale(1.1);
 }
-
-/* .main_container {
-    position: absolute;
-    top: 30%;
-    left: 50%;
-    margin-top: -50px;
-    margin-left: -50px;
-    width: 100px;
-    height: 100px;
-} */
-.album-heading-main{
-  display: block;
+.albums-heading-main {
+    display: block;
     font-size: 60px;
-    font-weight: 100;
-    padding-top: 3%;
+    font-weight: 60;
+    padding-top: 10%;
     padding-left: 30%;
     padding-bottom: 1%;
     color: Black;
-    font-family: 'Covered By Your Grace';
+    font-family: 'Montserrat';
+}
+#search_bar {
+  margin-bottom: 30px;
 }
 
-.albums_image {
+.albums_header {
   text-align: left;
   font-weight: bolder;
   padding-left: 5px;
   color: black;
+}
+
+.cancelSearch {
+  font-size: 14px;
+  text-align: right;
+}
+
+#albums_container {
+  padding-top: 10px;
+}
+.container_pagination {
+  padding-top: 10px;
+  text-align: center;
+}
+
+.container_pagination nav ul li a {
+  border: none;
+  background: transparent;
+}
+.page-link{
+  color: black;
+}
+#submit{
+  padding-top: 1%;
+}
+
+.btn{
+    float: right;
 }
 </style>
