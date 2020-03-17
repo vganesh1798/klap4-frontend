5a11b730027d79aff8c3e0d15cceaa52a2c2d39a
Sean Kennedy <sk747@umsystem.edu>
2020-03-17 15:21:21 -0500

    updated log

--- kmnr/src/views/PlaylistPage/PlaylistPage.vue
+++ kmnr/src/views/PlaylistPage/PlaylistPage.vue
@@ -1,21 +1,41 @@
 <template>
-    <div class="full-page">
-        <h1>Create New Log</h1>
-        <form>
-            <label for="name">Name</label>
-            <input type="text" id="name" />
+    <div class="full-page container">
+        <div class="row">
+            <h1 class="col s5 offset-s3 title">Create New Log</h1>
+        </div>
+        <div class="row">
+            <form class="col offset-s3 s6">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" />
+                    </div>
+                    <div class="input-field col s6">
+                        <label for="show">Show</label>
+                        <input type="text" id="show" />
+                    </div>
+                </div>
 
-            <label for="show">Show</label>
-            <input type="text" id="show" />
-
-            <label for="comment">Comment</label>
-            <input type="text" id="label" />
-
-            
-            <input type="text" />
-            <button type="submit">Create new Playlist</button>
-        </form>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <label for="comment">Comment</label>
+                        <input class="col lg6" type="text" id="label" />
+                    </div>
+                </div>
+                <button class="btn purple" type="submit">Create new Playlist</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script lang="ts" src="./PlaylistPage.ts" />
+
+<style lang="scss" scoped>
+    .full-page {
+        padding-top: 15%;
+    }
+
+    .title {
+        margin: 0px;
+    }
+</style>
