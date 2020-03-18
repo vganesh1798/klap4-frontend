bdc8856322caf01fc836449fffcce088de3b8f4a
Vasu Ganesh <vasu.ganesh09@gmail.com>
2020-03-17 21:10:02 -0500

    Shuffled READMEs

--- kmnr/README.md
+++ kmnr/README.md
@@ -17,16 +17,3 @@ yarn build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
-
-## Project Structure
-The `public/` directory contains the favicon and `index.html`, which has some CDN links for MateralizeCSS.
-
-The rest of the important files are located in the `src` folder:
-
-- `assets/` contains pictures.
-- `components/` contains small Vue components. These components will likely be single-file.
-- `router/` contains files for the router.
-- `store/` contains files for the Vuex store.
-- `views/` contains larger, page-sized Vue components.
-- `App.vue` is the root component.
-- `main.ts` is the Typescript file that compiles the root view, router, and store.
--- kmnr/src/views/Charts/Charts.ts
+++ kmnr/src/views/Charts/Charts.ts
@@ -1,6 +1,7 @@
 import { Component, Vue } from 'vue-property-decorator';
-import favbutton from "../../components/favbutton.vue";
 import axios from "axios";
+
+import favbutton from "../../components/favbutton.vue";
 
 @Component({
     components: {
@@ -31,39 +32,48 @@ export default class ChartsPage extends Vue {
             alert(error);
         });
     }
+
     hidePreviousBtn() {
         this.previousBtnVisibility = "hidden";
     }
+
     showPreviousBtn() {
         this.previousBtnVisibility = "visible";
     }
+
     hideNextBtn() {
         this.nextBtnVisibility = "hidden";
     }
+
     showNextBtn() {
         this.nextBtnVisibility = "visible";
     }
+
     nextCharts() {
         if (this.charts.length - this.range > 10) {
         this.range += 10;
         }
     }
+
     previousCharts() {
         if (this.range > 0) {
         this.range -= 10;
         }
     }
+
     SearchByChartName() {
         this.charts = this.charts.filter(alb => {
         return alb['category'].attributes.label.toLowerCase().includes(this.chartsSearch);
         });
         this.cancelSearchVisibility = "visible";
     }
+
     CancelSearch() {
         this.getCharts();
         this.chartsSearch = "";
         this.cancelSearchVisibility = "hidden";
     }
+    
     created() {
         this.getCharts();
       }
