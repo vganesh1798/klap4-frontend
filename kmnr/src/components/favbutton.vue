ecb542bfa2169b36bb08248e8880ad552fd641e0
Vasu Ganesh <vasu.ganesh09@gmail.com>
2020-03-17 19:11:55 -0500

    Updated charts page

---
+++ kmnr/src/components/favbutton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="favbutton">
+      <button class="btn-flat" @click="isToggled = !isToggled">
+        <i class="material-icons" v-show="!isToggled">star_border</i>
+        <i class="material-icons icon-yellow" v-show="isToggled">star</i>
+      </button>
+    </div>
+</template>
+
+<script lang='ts'>
+export default {
+  data(){
+    return{
+      isToggled: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
