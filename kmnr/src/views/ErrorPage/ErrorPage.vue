6f5d71994ed9810e7805ac48d253458520433cec
SercanAkcay_MST <sercan.akcay@mst.edu>
2020-04-28 20:38:03 -0500

    Create an error page

---
+++ kmnr/src/views/ErrorPage/ErrorPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="error-404 container">
+        <h2 style="text-align: center;">Error 404: Page Not Found!</h2>
+        <h1 style="text-align: center;">The page you were looking for does not exist!</h1>
+    </div>
+</template>
+
+<script lang="ts"/>
+
+<style lang="scss" scoped>
+    .error-404 {
+        padding-top: 15%;
+    }
+</style>
